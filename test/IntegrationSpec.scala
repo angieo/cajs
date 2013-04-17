a3c501bb2df87920111f596f38ac7acd02c048b3
Angelika Olejniczak <anesqo@gmail.com>
2013-04-17 11:55:37 +0200

    Fixed message in tests.

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -18,7 +18,7 @@ class IntegrationSpec extends Specification {
 
         browser.goTo("http://localhost:3333/")
 
-        browser.pageSource must contain("LOGO")
+        browser.pageSource must contain("Your new application is ready.")
        
       }
     }
